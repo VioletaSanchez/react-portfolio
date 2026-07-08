911133523871fac2fbaedc1064ad02770105b890
VioletaSanchez <violetasanchezpalacio@gmail.com>
2026-07-08 15:17:50 +0200

    portfolio deployment

---
+++ src/components/auth/login.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            errorText: ""
+        };
+
+        this.handleChamge = this.handleChamge.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChamge(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+            errorText: ""
+        });
+    }
+
+    handleSubmit(event) {
+        axios
+            .post(
+                "https://api.devcamp.space/sessions",
+                {
+                    client: {
+                        email: this.state.email,
+                        password: this.state.password
+                    }
+                },
+            { withCredentials: true }
+            ).then(response => {
+                if (response.data.status === "created") {
+                    this.props.handleSuccessfulAuth();
+                } else {
+                    this.setState({
+                        errorText: "Wrong email or password"
+                    });
+                    this.props.handleUnsuccessfulAuth();
+                }
+            })
+            .catch(error => {
+                this.setState({
+                    errorText: "An error occurred."
+                });
+                this.props.handleUnsuccessfulAuth();
+            });
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+
+                <div>{this.state.errorText}</div>
+
+                <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
+                    <div className="form-group">
+                        <FontAwesomeIcon icon="envelope" />
+                        <input
+                        type="email"
+                        name="email"
+                        placeholder="Your email"
+                        value={this.state.email}
+                        onChange={this.handleChamge}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <FontAwesomeIcon icon="lock" />
+                    <input
+                    type="password"
+                    name="password"
+                    placeholder="Your password"
+                    value={this.state.password}
+                    onChange={this.handleChamge}
+                    />
+                    </div>
+
+                    <button className="btn" type="submit">
+                        Login
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
